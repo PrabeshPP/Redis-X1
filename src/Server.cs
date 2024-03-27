a26205f10fc96c467a0335d419959763546a3459
Prabesh Bista <83439329+PrabeshPP@users.noreply.github.com>
2024-03-27 19:50:37 -0400

    test

--- src/Server.cs
+++ src/Server.cs
@@ -46,8 +46,14 @@ async void HandleMultipleConnection(Socket socket){
 }
 
 
+
 List<string> RedisReqParser(string request){
     string[] strList = request.Split("\r\n");
-    return strList.ToList();
+    if(strList[0][0]!='*'){
+        throw new Exception("Error in Encoded Message");
+    }
+
+    int length = int.Parse(strList[0][1..]);
+    return strList.Skip(1).Take(length*2).Where(x=>!x.StartsWith("$")).ToList();
 }
 
