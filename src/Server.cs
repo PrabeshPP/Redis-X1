b0a8f862ca6f017d5f6bd8f8b2d4bdeaaded6081
Prabesh Bista <83439329+PrabeshPP@users.noreply.github.com>
2024-03-27 13:26:58 -0400

    added'

--- src/Server.cs
+++ src/Server.cs
@@ -6,17 +6,25 @@ using System.Text;
 Console.WriteLine("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 6379);
+TcpListener server = new (IPAddress.Any, 6379);
 server.Start();
-Socket socket = server.AcceptSocket(); // wait for client
-
 
 string responseTxt = "+PONG\r\n";
 byte[] responseData = Encoding.UTF8.GetBytes(responseTxt);
 
-while(socket.Connected){
+while(true){
+    Socket socket = server.AcceptSocket();
+   await Task.Run(()=>HandleMultipleConnection(socket));
+}
+
+
+
+
+async void HandleMultipleConnection(Socket socket){
+    while(socket.Connected){
     byte[] buffer = new byte[1024];
     await socket.ReceiveAsync(buffer,SocketFlags.None);
     await socket.SendAsync(responseData,SocketFlags.None);
+    }
 }
 
